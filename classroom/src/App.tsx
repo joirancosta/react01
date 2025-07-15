576afd841631f8702f482eea59fa5e80b5f728c5
“Joiran <joiran.costa.jc@gmail.com>
2025-07-15 18:04:52 -0300

    innitial commit

---
+++ classroom/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/button"
+
+export function App(){
+    return (
+        // <> <h1>Hello World</h1> </>
+        <div>
+            <Button ></Button>
+            <Button />
+            <Button />
+        </div>
+    )
+}
