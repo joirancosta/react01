82fd5f6fc8023457ef1c490a208aa6e44f7e0e97
“Joiran <joiran.costa.jc@gmail.com>
2025-07-19 20:00:13 -0300

    add css

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -1,12 +1,17 @@
+import "./global.css"
 import { Button } from "./components/button"
+import styles from "./app.module.css"
 
 export function App(){
     return (
-        // <> <h1>Hello World</h1> </>
-        <div>
-            <Button ></Button>
-            <Button />
-            <Button />
+        //<>  
+        //  <h1>Hello World</h1>
+        //</>
+        <div className={styles.container}>
+            {/* <Button name="Criar" onClick={() => alert("Criar")}></Button> */}
+            <Button name="Adicionar" />
+            <span>0</span>
+            <Button name="Remover"/>
         </div>
     )
 }
