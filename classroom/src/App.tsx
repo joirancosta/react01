a385ba41fe15d417c78cd6720922639bcf10dfae
“joirancosta” <joiran.costa.jc@gmail.com>
2025-07-22 20:29:37 -0300

    add hooks

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -1,15 +1,20 @@
 import "./global.css"
 import { Button } from "./components/button"
+import { useMessage } from "./hooks/useMessage"
 import styles from "./app.module.css"
 
+
+
 export function App(){
+    const { show } = useMessage({age: 18, name: "Fulano"})
+
     return (
         //<>  
         //  <h1>Hello World</h1>
         //</>
         <div className={styles.container}>
             {/* <Button name="Criar" onClick={() => alert("Criar")}></Button> */}
-            <Button name="Adicionar" />
+            <Button name="Adicionar" onClick={() => show("Mensagem personalizada do meu Hook!")} />
             <span>0</span>
             <Button name="Remover"/>
         </div>
